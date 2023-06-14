dd835f974a82e4dd5a49ec8bc145294e50ebfbc9
Ida Naliaka <idahwafula@gmail.com>
2023-06-14 20:57:18 +0300

    Integrated Github api to fetch contributors

--- components/Aboutus/TeamCard.js
+++ components/Aboutus/TeamCard.js
@@ -5,8 +5,10 @@ import profile from "../../public/images/homepage/profile.png";
 const TeamCard = ({item}) => {
   return (
     <div className='w-fit hover:scale-105 cursor-pointer transition-transform'>
-        <Image src={(item?.img)?(item?.img):(profile)} alt="profile" height={200} width={200} />
-        {(item?.name)?(<h2 className='text-center'>{item?.name}</h2>):(<h2 className='text-center'>Username</h2>)}
+      <a href={item?.html_url} target= '_blank'>
+        <Image src={(item?.avatar_url)?(item?.avatar_url):(profile)} alt="profile" height={200} width={200} />
+        {(item?.login)?(<h2 className='text-center'>{item?.login}</h2>):(<h2 className='text-center'>Username</h2>)}
+    </a>
     </div>
   )
 }
