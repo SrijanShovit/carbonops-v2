e3b2007173fad4d312a0b6f95f556d7ed8a03965
Ida Naliaka <idahwafula@gmail.com>
2023-06-15 12:00:47 +0300

    Changed avatar styling

--- components/Aboutus/TeamCard.js
+++ components/Aboutus/TeamCard.js
@@ -6,7 +6,7 @@ const TeamCard = ({item}) => {
   return (
     <div className='w-fit hover:scale-105 cursor-pointer transition-transform'>
       <a href={item?.html_url} target= '_blank'>
-        <Image src={(item?.avatar_url)?(item?.avatar_url):(profile)} alt="profile" height={200} width={200} />
+        <Image src={(item?.avatar_url)?(item?.avatar_url):(profile)} alt="profile" height={200} width={200} className='rounded-full' />
         {(item?.login)?(<h2 className='text-center'>{item?.login}</h2>):(<h2 className='text-center'>Username</h2>)}
     </a>
     </div>
