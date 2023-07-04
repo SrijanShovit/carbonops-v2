6a6e69cd0cd262e2d56022baca05f9f3e7558c8d
Vishav Singla <95279293+VishavSingla@users.noreply.github.com>
2023-07-04 22:16:25 +0530

    Update mission statement in Banner.js
    Update mission statement to Our mission is to empower individuals and households to understand, reduce, and offset their carbon footprints through an easy-to-use online platform. We strive to provide accurate calculations, personalized recommendations, and educational resources, enabling users to make informed choices and take meaningful action toward a sustainable future.

--- components/Aboutus/Banner.js
+++ components/Aboutus/Banner.js
@@ -15,7 +15,7 @@ const Banner = () => {
         <div className="md:w-[40%]">
           <div className="flex flex-col justify-center md:items-start items-center gap-2">
             <h2 className="text-4xl font-bold mx-4">Our Mission</h2>
-            <p className="text-xl font-normal text-gray-200 mx-4 md:text-left text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet mollitia eos ducimus, beatae magnam voluptatem saepe tempora harum doloremque impedit ipsam, iste incidunt! Libero, suscipit dolorum quaerat quos vel odit.</p>
+            <p className="text-xl font-normal text-gray-200 mx-4 md:text-left text-center">Our mission is to empower individuals and households to understand, reduce, and offset their carbon footprints through an easy-to-use online platform. We strive to provide accurate calculations, personalized recommendations, and educational resources, enabling users to make informed choices and take meaningful action toward a sustainable future.</p>
           </div>
         </div>
       </div>
