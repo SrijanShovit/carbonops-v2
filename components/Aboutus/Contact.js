e2fc8b1514d1af37b98a5496aee783341493b317
Vishav Singla <95279293+VishavSingla@users.noreply.github.com>
2023-07-04 22:22:35 +0530

    Update who we are in Contact.js
    Update who we are in Contact.js

--- components/Aboutus/Contact.js
+++ components/Aboutus/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className='h-fit  border-2 mt-10 mx-[8vw] rounded-lg bg-[#5DAC53]'>
             <h2 className='p-4 text-2xl font-semibold text-white'>Who we are?</h2>
             <hr className='mx-4 mb-4 '/>
-            <p className='px-4 text-lg text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi maxime reprehenderit enim deserunt dicta, tempora iusto quae. Optio impedit labore autem reprehenderit quam, perspiciatis, at ad explicabo repellendus nesciunt minus?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe nemo odit consequuntur explicabo provident quidem esse optio nam iusto voluptate labore, tenetur deserunt sed! Quia doloribus nihil maiores harum vero.</p>
+            <p className='px-4 text-lg text-white'>We are CarbonOps, a dedicated team of environmental enthusiasts and technologists who have come together with a shared vision of combatting climate change. Our passion lies in empowering individuals and households to understand, reduce, and offset their carbon footprints through our user-friendly online platform. As advocates for sustainability, we firmly believe that every action, no matter how small, contributes to a larger collective impact. With that in mind, we have built CarbonOps to provide accessible tools, accurate calculations, and personalized recommendations, enabling our users to make informed choices and take meaningful action toward a sustainable future.</p>
             <button className='text-lg bg-white py-1 px-8 rounded-tl-xl rounded-br-xl rounded-tr-sm text-black rounded-bl-sm hover:shadow-md hover:shadow-green-300 duration-200 ease-out m-4'>Contact us</button>
         </div>
     </div>
