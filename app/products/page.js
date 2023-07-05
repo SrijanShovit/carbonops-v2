6fe83c96b6f8b9e822621c799af3a80c3a84966c
Mahendra Dani <danimahendra0904@gmail.com>
2023-07-05 08:26:04 +0530

    fix: resolved vercel errors

--- app/products/page.js
+++ app/products/page.js
@@ -1,28 +1,26 @@
 import React from "react";
 import Navbar from "@/components/products/Navbar";
 import Image from "next/image";
-import sampleImg from "../../public/images/homepage/documents.png";
-import yellowStar from "../../public/images/products/yellowStar.png";
-import grayStar from "../../public/images/products/grayStar.png";
 
 const page = () => {
+  const sampleImgUrl = "/images/homepage/documents.png";
   const products = [
     {
-      image: sampleImg,
+      image: sampleImgUrl,
       productName: "Product Name 1",
       description:
         " Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quis qui accusamus odit similique, expedita velit at cupiditate dolor iure? Laboriosam repudiandae quod pariatur necessitatibus expedita repellendus quidem cum repellat?",
       price: "₹100",
     },
     {
-      image: sampleImg,
+      image: sampleImgUrl,
       productName: "Product Name 2",
       description:
         " Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quis qui accusamus odit similique, expedita velit at cupiditate dolor iure? Laboriosam repudiandae quod pariatur necessitatibus expedita repellendus quidem cum repellat?",
       price: "₹150",
     },
     {
-      image: sampleImg,
+      image: sampleImgUrl,
       productName: "Product Name 3",
       description:
         " Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quis qui accusamus odit similique, expedita velit at cupiditate dolor iure? Laboriosam repudiandae quod pariatur necessitatibus expedita repellendus quidem cum repellat?",
@@ -46,7 +44,7 @@ const page = () => {
               <div className="w-[85%] mx-auto bg-[#12323f] flex flex-col sm:flex-row justify-between  sm:justify-between items-center gap-2 sm:px-4 py-2">
                 <div className="sm:flex-1">
                   <Image
-                    src={sampleImg}
+                    src={sampleImgUrl}
                     height={300}
                     width={300}
                     alt="sample img"
@@ -63,31 +61,31 @@ const page = () => {
                     <div className="hidden sm:flex justify-between items-center gap-12">
                       <div className="flex flex-row justify-between items-center">
                         <Image
-                          src={yellowStar}
+                          src={"/images/products/yellowStar.png"}
                           height={25}
                           width={25}
                           alt="star"
                         />
                         <Image
-                          src={yellowStar}
+                          src={"/images/products/yellowStar.png"}
                           height={25}
                           width={25}
                           alt="star"
                         />
                         <Image
-                          src={yellowStar}
+                          src={"/images/products/yellowStar.png"}
                           height={25}
                           width={25}
                           alt="star"
                         />
                         <Image
-                          src={yellowStar}
+                          src={"/images/products/yellowStar.png"}
                           height={25}
                           width={25}
                           alt="star"
                         />
                         <Image
-                          src={grayStar}
+                          src={"/images/products/grayStar.png"}
                           height={25}
                           width={25}
                           alt="star"
@@ -121,30 +119,35 @@ const page = () => {
                   <div className="w-[20rem] flex justify-between items-center">
                     <div className="flex flex-row justify-between items-center">
                       <Image
-                        src={yellowStar}
+                        src={sampleImgUrl}
                         height={25}
                         width={25}
                         alt="star"
                       />
                       <Image
-                        src={yellowStar}
+                        src={"/images/products/yellowStar.png"}
                         height={25}
                         width={25}
                         alt="star"
                       />
                       <Image
-                        src={yellowStar}
+                        src={"/images/products/yellowStar.png"}
                         height={25}
                         width={25}
                         alt="star"
                       />
                       <Image
-                        src={yellowStar}
+                        src="/images/products/yellowStar.png"
                         height={25}
                         width={25}
                         alt="star"
                       />
-                      <Image src={grayStar} height={25} width={25} alt="star" />
+                      <Image
+                        src={"/images/products/grayStar.png"}
+                        height={25}
+                        width={25}
+                        alt="star"
+                      />
                     </div>
                     <div className="text-orange-400">{product.price}</div>
                   </div>
