0cc0134a0128972edb451bda46be5792f372d7d3
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-07-09 13:35:49 +0530

    Review Changes 2

--- src/app/URL.js
+++ src/app/URL.js
@@ -1,2 +1,5 @@
-export const backendURL = "http://localhost:5000" // You local backend url
-export const frontendURL = "http://localhost:3000" // You local backend url
+export const backendURL = "http://localhost:5000" // Add backend URl here 
+//ex carbonops.onrendre.com, don't add the root slash (/) i.e carbonops.onrendre.com/
+
+export const frontendURL = "https://carbonops-v2.vercel.app" 
+// export const frontendURL = "http://localhost:3000" // You local frontendURl url
