103bf7f3f5a84d5f216b209cd752a6867fdf46e7
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2023-08-10 08:48:42 +0530

    added card in team

--- src/components/Aboutus/TeamCard.js
+++ src/components/Aboutus/TeamCard.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Image from 'next/image';
 
-const TeamCard = ({item}) => {
+const TeamCard = ({ item }) => {
   return (
-    <div className='w-fit hover:scale-105 hover:opacity-70 cursor-pointer transition-transform'>
-      <a href={item?.html_url} target= '_blank'>
-        <Image src={(item?.avatar_url)?(item?.avatar_url):(profile)} alt="profile" height={200} width={200} className='rounded-full transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/10' />
-        {(item?.login)?(<h2 className='text-center'>{item?.login}</h2>):(<h2 className='text-center'>Username</h2>)}
-    </a>
-
+    <div className='w-fit hover:scale-105 hover:opacity-70 cursor-pointer transition-transform border-2 border-gray py-10 px-10'>
+      <a href={item?.html_url} target='_blank'>
+        <Image
+          src={(item?.avatar_url) ? (item?.avatar_url) : (profile)}
+          alt="profile" height={200} width={200} className='rounded-full transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/10' />
+        {(item?.login) ? (<h2 className='text-center text-xl'>{item?.login}</h2>) : (<h2 className='text-center '>Username</h2>)}
+      </a>
+      <p className='text-center' > {` ${item.contributions} commits`}</p>
     </div>
   );
 }
