8dd49a9dd8959957ba1ea0decee1cf7aadd3fa7f
MeetCodes10 <shishui75418@gmail.com>
2023-08-16 21:11:23 +0530

    Revamped the feedback/contactus page and made it mobile responsive

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -7,49 +7,48 @@ function Contact() {
   const instagramColor = "#E4405F"; // Instagram Pink
   const linkedinColor = "#0077B5";
   return (
-    <div className="mt-24">
-      
-      <div className="mt-[10rem] ml-[0rem] sm:ml-[3rem] mb-[4rem]">
-        <div className="ml-[13%]">
-          <button
-            href="#"
-            class="feedback-button font-extrabold bg-green-500 text-xl cursor-default hover:bg-green-600 text-white  py-2 px-4 rounded"
-          >
-            Feedback
-          </button>
-          <p className="text-white text-2xl mt-[2rem]">
-            Please share your feedback. It is highly appreciated!
-          </p>
-        </div>
+    <div className="mt-14">
+      <div>
+      <div className="bg-[transparent] py-6 sm:py-8 lg:py-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <div className="mb-10 md:mb-16">
+            <h2 className="mb-4 text-white text-center text-2xl font-bold  md:mb-6 lg:text-3xl">Please share your feedback it is highly appreciated</h2>
 
-        <div className="ml-[13%] my-12 w-[75%]  max-w-2xl">
-          <div>
+            <p className="mx-auto max-w-screen-md text-center text-gray-100 md:text-lg">Welcome to our feedback page! We value your opinions and insights, as they help us improve and provide you with a better experience. Whether you have suggestions, questions, or comments about our products, services, or website, this is the place to share them.</p>
+          </div>
+          <form className="mx-auto grid max-w-screen-md gap-4 sm:grid-cols-2">
+            <div>
+              <label for="first-name" className="mb-2 inline-block text-sm  sm:text-base">First name*</label>
+              <input name="first-name" className="w-full rounded border bg-gray-50 px-3 py-2  outline-none ring-indigo-300 transition duration-100 focus:ring" />
+            </div>
 
-          <textarea
-            className=" w-full feedback-textarea bg-lightgreen-200 resize-none border bg-green-200 border-gray-300 text-black rounded-md p-2 h-[14rem] mb-2"
-            placeholder="Enter your feedback"
-            ></textarea>
-          </div>
-          <div>
-          <button className=" float-right bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-            Submit Feedback
-          </button>
-          </div>
+            <div>
+              <label for="last-name" className="mb-2 inline-block text-sm text-white sm:text-base">Last name*</label>
+              <input name="last-name" className="w-full rounded border bg-gray-50 px-3 py-2 text-white outline-none ring-indigo-300 transition duration-100 focus:ring" />
+            </div>
+
+           
+
+            <div className="sm:col-span-2">
+              <label for="email" className="mb-2 inline-block text-sm text-white sm:text-base">Email*</label>
+              <input name="email" className="w-full rounded border bg-gray-50 px-3 py-2 text-white outline-none ring-indigo-300 transition duration-100 focus:ring" />
+            </div>
+
+
+            <div className="sm:col-span-2">
+              <label for="message" className="mb-2 inline-block text-sm text-white sm:text-base">Feedback*</label>
+              <textarea name="message" className="h-64 w-full rounded border bg-gray-50 px-3 py-2 text-white outline-none ring-indigo-300 transition duration-100 focus:ring"></textarea>
+            </div>
+
+            <div className="flex items-center justify-between sm:col-span-2">
+              <button className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">Submit Feedback</button>
+
+            </div>
+
+            
+          </form>
         </div>
-        <div className="ml-[12%] mt-12">
-          <a
-            href="#"
-            className="contact-button bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-          >
-            Contact Us
-          </a>
-        </div>
-        <div className="ml-[13%] mt-[2rem]  flex  sm:flex-col space-x-8 sm:space-x-0 sm:space-y-2 justify-center">
-          <AiOutlineMail size={24} color={emailColor} />
-          <FaTwitter size={24} color={twitterColor} />
-          <FaInstagram size={24} color={instagramColor} />
-          <FaLinkedinIn size={24} color={linkedinColor} />
-        </div>
+      </div>
       </div>
     </div>
   );
